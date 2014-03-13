956f7392250d3bc1e198c5da2524e49150e95cd0
wise_joker <jayzjj000@163.com>
2014-03-13 18:56:23 +0800

    Initial Commit

---
+++ CharRoom/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CharRoom
+//
+//  Created by 朱 俊健 on 14-3-13.
+//  Copyright (c) 2014年 朱 俊健. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ CharRoomTests/CharRoomTests.m
@@ -0,0 +1,34 @@
+//
+//  CharRoomTests.m
+//  CharRoomTests
+//
+//  Created by 朱 俊健 on 14-3-13.
+//  Copyright (c) 2014年 朱 俊健. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CharRoomTests : XCTestCase
+
+@end
+
+@implementation CharRoomTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
